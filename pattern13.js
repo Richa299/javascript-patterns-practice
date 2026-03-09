b789c370e52fe6a7743c1d735ae36f29b93188e5
Richa Singh <richa.singhnxt213@gmail.com>
2026-03-09 17:57:39 +0530

    added-pattern-13-practice

---
+++ pattern13.js
@@ -0,0 +1,16 @@
+let n = 5;
+let star = "";
+for (let i = 1; i <= n; i++) {
+  for (let j = 1; j <= n; j++) {
+    if (i == 1 || i == n) {
+      star += "*";
+    } else if (j == 1 || j == n) {
+      star += "*";
+    } else {
+      star += " ";
+    }
+  }
+
+  star += "\n";
+}
+console.log(star);
